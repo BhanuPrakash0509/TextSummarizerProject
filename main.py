7fc4be9b17a69d8c47503b28db2d5486d69251d0
BhanuPrakash0509 <bhanuprakash0509@gmail.com>
2024-02-12 14:19:05 +0530

    data ingestion pipline

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+from textSummarizer.pipeline.stage01_data_ingestion import DataIngestionTrainingPipline
+
 from textSummarizer.logging import logger
 
-logger.info("this is a custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
---
+++ src/textSummarizer/pipeline/stage01_data_ingestion.py
@@ -0,0 +1,18 @@
+from textSummarizer.components.data_ingestion import DataIngestion
+from textSummarizer.config.configuration import ConfiguarationManager
+from textSummarizer.logging import logger
+
+
+
+class DataIngestionTrainingPipline:
+    def __init__(self) -> None:
+        pass
+
+    def main(self):
+        config = ConfiguarationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+    
+ 
