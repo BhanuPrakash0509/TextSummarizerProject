595832c9255672ca4356efccde9f7e4bab7757f1
BhanuPrakash0509 <bhanuprakash0509@gmail.com>
2024-02-13 15:48:39 +0530

    model evaluation & prediction

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from textSummarizer.pipeline.stage01_data_ingestion import DataIngestionTraining
 from textSummarizer.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 
 from textSummarizer.logging import logger
+from textSummarizer.pipeline.stage03_data_transformation import DataTransformationTrainingPipeline
+from textSummarizer.pipeline.stage04_model_trainer import ModelTrainerTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion stage"
@@ -20,6 +22,27 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_validation = DataValidationTrainingPipeline()
    data_validation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Model Trainer stage"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainerTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
---
+++ src/textSummarizer/pipeline/stage04_model_trainer.py
@@ -0,0 +1,14 @@
+from textSummarizer.config.configuration import ConfiguarationManager
+from textSummarizer.components.model_trainer import ModelTrainer
+from textSummarizer.logging import logger
+
+
+class ModelTrainerTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfiguarationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
